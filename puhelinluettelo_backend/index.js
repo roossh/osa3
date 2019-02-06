b2c86d72a4d67162f7b6039319a7b278752c3535
roossh <roossh@users.noreply.github.com>
2019-02-06 22:11:45 +0200

    3.1

---
+++ puhelinluettelo_backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "045-1236543"
+    },
+    {
+        id: 2,
+        name: "Arto Järvinen",
+        number: "041-21423123"
+    },
+    {
+        id: 3,
+        name: "Lea Kutvonen",
+        number: "040-4323234"
+    },
+    {
+        id: 4,
+        name: "Martti Tienari",
+        number: "09-784232"
+    }
+]
+
+app.use(bodyParser.json())
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
