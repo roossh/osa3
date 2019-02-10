8d4f1936a4e9c5d14ee8a06f7558b0cec679a9c9
roossh <roossh@users.noreply.github.com>
2019-02-10 17:56:51 +0200

    3.20

--- build/precache-manifest.9b85f3565f19ff55695ed0f75d6721ce.js
+++ build/precache-manifest.9b85f3565f19ff55695ed0f75d6721ce.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "d37d44d99d35231ca2d1",
-    "url": "/static/js/main.d37d44d9.chunk.js"
+    "revision": "123aa1180b1823d25ffe",
+    "url": "/static/js/main.123aa118.chunk.js"
   },
   {
     "revision": "1bdeb50c48bfe976fdd4",
     "url": "/static/js/1.1bdeb50c.chunk.js"
   },
   {
-    "revision": "5ea0c2a9d70997ab47b38635bceed213",
+    "revision": "d6d848bc79ffa0dd98e87c6a28b7f1c3",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.492f73e25ccedd7b1b2957b780dfb1a9.js"
+  "/precache-manifest.9b85f3565f19ff55695ed0f75d6721ce.js"
 );
 
 workbox.clientsClaim();
